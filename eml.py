9419f61b0aeebf70ba112aa988a4c28dc81ef3b8
test <abiram98@gmail.com>
2022-03-10 11:10:51 +0100

    relations added

--- eml.py
+++ eml.py
@@ -11,10 +11,10 @@ def load_CSKG():
    return nodes,relations
 
 # Assumes conceptnet 5.5 / 5.7 source file in conceptnet folder
-def load_ConceptNet5():
+def load_ConceptNet5(path):
    # The graph is available at: https://github.com/commonsense/conceptnet5/wiki/Downloads
    # It will be downloaded as .csv but is tab-seperated
-   df = pd.read_csv('ConceptNet/assertions.tsv',sep='\t', error_bad_lines=False)
+   df = pd.read_csv(path,sep='\t', error_bad_lines=False)
    relations = set(df.iloc[:,1])
    node1s = set(df.iloc[:,2])
    node2s = set(df.iloc[:,3])
@@ -45,3 +45,6 @@ def el(em, nodelist):
        if em in node:
            el_list.append(node)
    return el_list
+if __name__ == "__main__":
+    _,relations = load_ConceptNet5('ConceptNet/conceptnet-assertions-5.7.0.csv')
+    print(relations)
